d410e41ee6c399ebdb202056217f8800cb113a60
kisshour <kisshour@gmail.com>
2026-01-28 23:21:41 +0900

    Add colored logo to Brand page

--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useLanguage } from '../context/LanguageContext';
 
+import logoColor from '../assets/logo_color.png';
+
 const Brand = () => {
     const { content } = useLanguage();
 
     return (
-        <div className="container" style={{ paddingTop: '150px', maxWidth: '800px' }}>
+        <div className="container" style={{ paddingTop: '150px', maxWidth: '800px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <img src={logoColor} alt="HARU Logo" style={{ width: '120px', marginBottom: '30px' }} />
             <h1 style={{ fontSize: '2.5rem', marginBottom: '40px', textAlign: 'center' }}>{content.brandStory.title}</h1>
 
             <div style={{ fontSize: '1.1rem', lineHeight: '1.8', color: '#444' }}>
